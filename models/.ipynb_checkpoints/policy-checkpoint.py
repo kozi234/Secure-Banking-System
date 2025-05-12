b88221cbb19d64be0e53490534be6eb60ddc1a94
kozi234 <kiboziboy234@gmail.com>
2025-05-11 21:53:28 -0600

    Delete models directory

--- models/.ipynb_checkpoints/policy-checkpoint.py
+++
@@ -1,4 +0,0 @@
-class SecurityPolicy:
-    MIN_PASSWORD_LENGTH = 10
-    PASSWORD_COMPLEXITY_REGEX = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$"
-    MAX_TRANSACTION_LIMIT = 5000.00
